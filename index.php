1e20f2fbc5fd11e7943ec4537e10629af5c0b290
PetitZboub <enzorouch31@gmail.com>
2023-05-10 08:09:28 +0200

    Image angel modifier

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ $requete->closeCursor();
 	<base target="_blank" />
 	<meta name="author" content="Onyx" />
 	<meta charset="UTF-8" />
-	<title>Lecteur de musique</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 </head>
 
