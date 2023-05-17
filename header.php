98c3eb95b9af4f852529adc6d33d245e25169ff8
PetitZboub <enzorouch31@gmail.com>
2023-05-17 13:00:02 +0200

    barre de recherche

--- header.php
+++ header.php
@@ -4,10 +4,6 @@
     <img src="img/logo.png" alt="Logo">
   </div>
   <div class="search">
-    <form>
-      <input type="text" placeholder="Rechercher...">
-      <button type="submit">Rechercher</button>
-    </form>
   </div>
   <nav>
     <ul>
